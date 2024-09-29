856baff057738c950ec494076012711091a07d46
RoxanaDC <cretu2roxana@gmail.com>
2024-09-29 23:28:27 +0200

    refactor:erase unuseful Test component

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -4,7 +4,6 @@ import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import Error from "./Pages/Error/Error";
 import Logement from "./Pages/Logement/Logement";
-import Test from "./Pages/Test/Test";
 
 const AppRouter = () => (
   <Routes>
@@ -12,7 +11,6 @@ const AppRouter = () => (
     <Route path="/about" element={<About />} />
     <Route path="/logement/:id" element={<Logement />} />
     <Route path="*" element={<Error />} />
-    <Route path="/test" element={<Test />} />
   </Routes>
 );
 
