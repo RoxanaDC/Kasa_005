be33811bee227e36243cb01faf7cf6425d6e25d3
RoxanaDC <cretu2roxana@gmail.com>
2024-09-29 23:22:57 +0200

    refactor:AppRouter-arrow function

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -6,15 +6,14 @@ import Error from "./Pages/Error/Error";
 import Logement from "./Pages/Logement/Logement";
 import Test from "./Pages/Test/Test";
 
-function AppRouter() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/logement/:id" element={<Logement />} />
-      <Route path="*" element={<Error />} />
-      <Route path="/test" element={<Test />} />
-    </Routes>
-  );
-}
+const AppRouter = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/logement/:id" element={<Logement />} />
+    <Route path="*" element={<Error />} />
+    <Route path="/test" element={<Test />} />
+  </Routes>
+);
+
 export default AppRouter;
