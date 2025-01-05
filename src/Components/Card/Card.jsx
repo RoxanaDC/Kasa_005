0d8454bbdffa8f45f3fac5679b01f30894b9d28d
RoxanaDC <cretu2roxana@gmail.com>
2025-01-05 15:16:50 +0100

    actualizations for deployment

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Card.scss";
 
-//le composant a "title", "cover" et "id" en tant que "prop"-s
-const Card = ({ title, cover, id }) => (
+//le composant a "title",
+//"cover" et "id" en tant que "prop"-s
+const Card = ({ id, title, cover }) => (
   <Link className="card" to={`/logement/${id}`}>
     <h3 className="card__title">{title}</h3>
     <img className="card__image" src={cover} alt={title} />
