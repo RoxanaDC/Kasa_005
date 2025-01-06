52d36565b82f2b8d3c119d335dfd6b3b8f9ae5b7
RoxanaDC <cretu2roxana@gmail.com>
2025-01-06 02:57:36 +0100

    alt stars = *

--- src/Components/Stars/Stars.jsx
+++ src/Components/Stars/Stars.jsx
@@ -1,33 +1,25 @@
-import React from "react";
-import "./Stars.scss";
+import React from 'react';
+import './Stars.scss';
 
 function Stars({ rating }) {
   const stars = [];
   for (let i = 1; i <= 5; i++) {
     if (i <= rating) {
       stars.push(
-        <span className="star star--full" key={i}>
-          <img
-            className="star__icone"
-            src="/star-active.png"
-            alt="icone_rating"
-          />
+        <span className='star star--full' key={i}>
+          <img className='star__icone' src='/star-active.png' alt='⁎' />
         </span>
       );
     } else {
       stars.push(
-        <span className="star star--empty" key={i}>
-          <img
-            className="star__icone"
-            src="/star-inactive.png"
-            alt="icone_rating"
-          />
+        <span className='star star--empty' key={i}>
+          <img className='star__icone' src='/star-inactive.png' alt='*' />
         </span>
       );
     }
   }
 
-  return <div className="stars">{stars}</div>;
+  return <div className='stars'>{stars}</div>;
 }
 
 export default Stars;
