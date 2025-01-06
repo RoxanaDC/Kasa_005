f2ea5921e11862599b8e98882b4a06480e03ccb0
RoxanaDC <cretu2roxana@gmail.com>
2025-01-06 01:49:13 +0100

    logo.png

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import "./Header.scss";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import './Header.scss';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
-  <header className="header">
-    <img className="header__logo" src="../logo.svg" alt="Logo_Kasa" />
-    <nav className="header__navbar">
-      {["/", "/about"].map((path, index) => (
+  <header className='header'>
+    <img className='header__logo' src='../logo.png' alt='Logo_Kasa' />
+    <nav className='header__navbar'>
+      {['/', '/about'].map((path, index) => (
         <NavLink
           key={index}
           className={({ isActive }) =>
-            `header__navbar__link${isActive ? " active" : ""}`
+            `header__navbar__link${isActive ? ' active' : ''}`
           }
           to={path}
         >
-          {path === "/" ? "Accueil" : "A propos"}
+          {path === '/' ? 'Accueil' : 'A propos'}
         </NavLink>
       ))}
     </nav>
